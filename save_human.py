6cca6fb40748916c263c463e3382c7d4b31c05f8
tombo314 <yuto.kidoba@geniee.co.jp>
2026-06-23 14:17:53 +0900

    🗃️ 住民のDBをrename

--- save_human.py
+++ save_human.py
@@ -2,7 +2,7 @@ from human import Human
 import json
 
 def save_human(people: list[Human]):
-    with open("database/human.json", "w", encoding="utf-8") as f:
+    with open("database/residents.json", "w", encoding="utf-8") as f:
         save_data: dict[str, dict[str, dict[str, str | int]]] = {"people": dict()}
         for human in people:
             save_data["people"][human.name] = dict()
