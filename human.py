06c1956b2ee4eb3d52698e9dbe0c43b4aa35907a
tombo314 <yuto.kidoba@geniee.co.jp>
2026-06-23 14:22:18 +0900

    ✨ Humanクラスにprint用メソッドを定義

--- human.py
+++ human.py
@@ -2,3 +2,6 @@ class Human:
     def __init__(self, name: str, money: int) -> None:
         self.name: str = name
         self.money: int = money
+
+    def __repr__(self) -> str:
+        return f"名前: {self.name}"
