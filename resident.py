feff4043902acb3fedd9929d36e653f35bb4444f
tombo314 <yuto.kidoba@geniee.co.jp>
2026-06-25 18:47:46 +0900

    ✨ 住民を修正

--- resident.py
+++ resident.py
@@ -1,9 +1,9 @@
 from human import Human
 
 kidoba = Human("Kidoba Yuto", 1000)
-fujiwara = Human("Yuya fujiwara", 2000)
+suzuki = Human("Jiro Suzuki", 2000)
 
 residents : list[Human] = [
     kidoba,
-    fujiwara
+    suzuki
 ]
