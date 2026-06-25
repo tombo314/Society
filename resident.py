1ffffb008f40bbf1b33752fe77131970c529a682
tombo314 <yuto.kidoba@geniee.co.jp>
2026-06-25 18:20:20 +0900

    🎨 ファイルのリネーム、人の追加

---
+++ resident.py
@@ -0,0 +1,9 @@
+from human import Human
+
+kidoba = Human("Kidoba Yuto", 1000)
+fujiwara = Human("Yuya fujiwara", 2000)
+
+residents : list[Human] = [
+    kidoba,
+    fujiwara
+]
--- residents.py
+++
@@ -1,8 +0,0 @@
-from human import Human
-
-kidoba = Human("yuto-kidoba", 1000)
-
-residents : list[Human] = [
-    kidoba,
-    kidoba
-]
