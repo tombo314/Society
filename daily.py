3b3bf2707b42da1969430fcffbb5c8d73c1b0aab
tombo314 <yuto.kidoba@geniee.co.jp>
2026-06-26 15:07:58 +0900

    ✨ Dailyクラスの概要を作成

---
+++ daily.py
@@ -0,0 +1,12 @@
+class Daily:
+    def __init__(self) -> None:
+        self.daily_action: dict[str, list[str]] = dict()
+
+    def get_morning_action(self):
+        pass
+
+    def get_afternoon_action(self):
+        pass
+    
+    def get_evening_action(self):
+        pass
