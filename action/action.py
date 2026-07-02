00142b105c0571a55b800bc763ea64800233449f
tombo314 <u_tombo@icloud.com>
2026-07-03 01:18:17 +0900

    chore: 表示・コメント・日数ロジックの修正

--- action/action.py
+++ action/action.py
@@ -4,13 +4,13 @@ from people.human import Human
 
 class Action:
     def __init__(self) -> None:
-        # action_id: action
         self.actions: list[str] = [
             "何もしなかった。",
             "朝ご飯を食べた。"
         ]
 
     def act_randomly(self, person: Human) -> Human:
+        # 行動をランダムに決定する
         random_action_id = randint(0, len(self.actions) - 1)
         person.action_log = self.actions[random_action_id]
         person = self._act(person, random_action_id)
@@ -24,6 +24,8 @@ class Action:
             case _:
                 # 何もしない
                 pass
+        # 1行動で1日経過する
+        person.days_passed += 1
         return person
 
     def have_meal(self, person: Human) -> Human:
@@ -31,5 +33,5 @@ class Action:
         SATIETY_LIMIT: int = 100
         person.satiety = min(SATIETY_LIMIT, person.satiety + SATIETY_INCREMENT)
         if person.satiety >= SATIETY_LIMIT:
-            person.action_log += f"{person.name} は満腹になった。"
+            person.action_log += f"{person.name} はお腹いっぱいになった。"
         return person
